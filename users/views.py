6811e3dda2ecab40be7fb4560d1466b717eb6c29
nevinwu <f.martinezpico@gmail.com>
2024-11-14 16:06:33 +0100

    Minor changes

--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth import update_session_auth_hash, login
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 from django.shortcuts import render, redirect
-from django.shortcuts import render, redirect
 from donations.models import Donacion
 from .models import Profile
 from .forms import UserRegisterForm, ProfileUpdateForm, CustomPasswordChangeForm
